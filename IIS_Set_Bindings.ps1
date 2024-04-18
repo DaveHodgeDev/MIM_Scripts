1967b2e959bf772e149eb7ce071b8ffbe98a6ad9
DaveHodgeDev <47575373+DaveHodgeDev@users.noreply.github.com>
2024-04-18 09:52:27 -0400

    Create IIS_Set_Bindings.ps1

---
+++ IIS_Set_Bindings.ps1
@@ -0,0 +1,3 @@
+# https://learn.microsoft.com/en-us/iis/manage/powershell/powershell-snap-in-configuring-ssl-with-the-iis-powershell-snap-in
+
+#PS IIS:\> New-WebBinding -Name "Default Web Site" -IP "*" -Port 443 -Protocol https
