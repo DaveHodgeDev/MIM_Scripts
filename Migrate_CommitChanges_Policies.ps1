832c9a87b03fafba2c235026a113708570d2a827
DaveHodgeDev <47575373+DaveHodgeDev@users.noreply.github.com>
2024-05-06 08:37:02 -0400

    Add files via upload

---
+++ Migrate_CommitChanges_Policies.ps1
@@ -0,0 +1,35 @@
+##############################################################################################################
+# CommitChanges.ps1
+# Copyright © 2009 Microsoft Corporation
+#
+# This script applies the changes necessary to migrate configuration.
+##############################################################################################################
+
+if(@(get-pssnapin | where-object {$_.Name -eq "FIMAutomation"} ).count -eq 0) {add-pssnapin FIMAutomation}
+
+$changes_filename = "d:\code\base\serviceconfig\policychanges.xml"
+$undone_filename = "d:\code\base\serviceconfig\policychanges_undone.xml"
+
+$imports = ConvertTo-FIMResource -file $changes_filename
+
+if($imports -eq $null)
+{
+    throw (new-object NullReferenceException -ArgumentList "Changes is null.  Check that the changes file has data.")
+}
+
+Write-Host "Importing changes into production."
+
+$undoneImports = $imports | Import-FIMConfig
+
+if($undoneImports -eq $null)
+{
+    Write-Host "Import complete."
+}
+else
+{
+    Write-Host
+    Write-Host "There were " $undoneImports.Count " uncompleted imports."
+    $undoneImports | ConvertFrom-FIMResource -file $undone_filename
+    Write-Host
+    Write-Host "Please see the documentation on how to resolve the issues."
+}
